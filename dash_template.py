698e71b1407cb66ffe55e5f60f6b8f52802532e0
Charlie DeVries <74932234+csdevrie@users.noreply.github.com>
2023-01-21 14:23:25 -0600

    Charlie's Pie and Sankey with a touch of map

--- dash_template.py
+++ dash_template.py
@@ -1,12 +1,15 @@
 import dash
 import dash_bootstrap_components as dbc
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc
+from dash import html
 from dash.dependencies import Input, Output, State
 import pandas as pd
 import seaborn as sns
 import plotly.express as px
 import dash_leaflet as dl
+import ssl
+import math
+ssl._create_default_https_context = ssl._create_unverified_context
 
 # the style arguments for the sidebar.
 SIDEBAR_STYLE = {
@@ -89,11 +92,17 @@ content_first_row = dbc.Row([
         md=3
     )
 ])
-
+# SUCCESS
 # EXTRACTING DATA FOR PIE CHART, USED FOR PRISONER'S DILEMMA
-df = px.data.gapminder().query("year == 2007").query("continent == 'Europe'")
-df.loc[df['pop'] < 2.e6, 'country'] = 'Other countries' # Represent only large countries
-prison_dec_fig = px.pie(df, values='pop', names='country', title='Prisoner\'s Dilemma Challenge Decisions')
+df = pd.read_csv('data/Boilermake Fake Data - Player.csv')
+count_dict = dict()
+df.loc[df['IsBetrayer'] == 1] = 'Betrayer'
+df.loc[df['IsBetrayer'] == 0] = 'Honest'
+dfvals = df['IsBetrayer'].value_counts().to_frame()
+dfvals.reset_index(inplace=True)
+dfvals = dfvals.rename(columns = {'index':'Name'})
+
+prison_dec_fig = px.pie(dfvals, values='IsBetrayer', names='Name', title='Prisoner\'s Dilemma Challenge Decisions')
 prison_dec_fig.update_layout(title_x=0.5)
 
 # EXTRACTING DATA FOR SANKEY DIAGRAM, USED FOR CHALLENGE PROGRESSION
@@ -101,20 +110,53 @@ prison_dec_fig.update_layout(title_x=0.5)
 import plotly.graph_objects as go
 import urllib, json
 
-url = 'https://raw.githubusercontent.com/plotly/plotly.js/master/test/image/mocks/sankey_energy.json'
-response = urllib.request.urlopen(url)
-data = json.loads(response.read()) # replace with your own data source
 
-node = data['data'][0]['node']
-node['color'] = [
-    f'rgba(255,0,255,0.8)' 
-    if c == "magenta" else c.replace('0.8', '0.9') 
-    for c in node['color']]
+frags = pd.read_csv('data/Boilermake Fake Data - CollectedDataFragment.csv')['DataFragmentId'].to_list()
+print(frags)
+idx = max(frags) * 2
+labels = []
+for i in range(0, max(frags) + 1):
+    labels.append('Pass Task ' + str(i))
+    labels.append('Fail Task ' + str(i))
+print('LABELS:', labels)
+sources = []
+for i in range(0, idx + 1):
+    sources.append(math.floor(i / 2) * 2)
 
-link = data['data'][0]['link']
-link['color'] = [node['color'][src] for src in link['source']]
+print('SOURCES', sources)
 
-sankey_event_completion_fig = go.Figure(go.Sankey(link=link, node=node))
+targets = []
+for i in range(0, idx + 1):
+    targets.append(i + 2)
+print('TARGETS:', targets)
+
+values = []
+counts = dict()
+for i in frags:
+    counts[i] = 0
+for i in frags:
+    counts[i]+=1
+print('COUNTS:', counts)
+for x in counts:
+    values.append(counts[x])
+    if x == 0:
+        values.append(0)
+    else:
+        values.append(counts[x-1] - counts[x])
+print('VALUES:', values)
+sankey_event_completion_fig = fig = go.Figure(data=[go.Sankey(
+    node = dict(
+      pad = 15,
+      thickness = 20,
+      line = dict(color = "black", width = 0.5),
+      label = labels,
+      color = "blue"
+    ),
+    link = dict(
+      source = sources, # indices correspond to labels, eg A1, A2, A1, B1, ...
+      target = targets,
+      value = values
+  ))])
 sankey_event_completion_fig.update_layout(title_text="Challenge Progression", title_x=0.5)
 
 # FIRST ROW OF GRAPHS
@@ -141,18 +183,25 @@ content_third_row = dbc.Row([
 ])
 
 # NFC TAG MAP OF TAG STATUS
+df = pd.read_csv('data/Boilermake Fake Data - DataFragment.csv')
+lon_lat_df = df[['Name', 'Latitude', 'Longitude']].to_dict()
+tag_locations = []
+for i in range(len(lon_lat_df['Latitude'])):
+    tag_locations.append([lon_lat_df['Name'][i],lon_lat_df['Latitude'][i], lon_lat_df['Longitude'][i]])
+
 markers = []
-tag_locations = [[77, -119], [11, 151], [-94, 51], [-139, -138], [-178, -139], [77, 146], [-80, 28], [-11, -118]]
+# tag_locations = [[77, -119], [11, 151], [-94, 51], [-139, -138], [-178, -139], [77, 146], [-80, 28], [-11, -118]]
 for tl in tag_locations:
     markers.append(
         dl.Marker(
-            position=(tl[0], tl[1]),
+            title=tl[0],
+            position=(tl[1], tl[2]),
         )
     )
 content_fourth_row = dbc.Row([
     dbc.Col([
         html.H4('NFC Tag Status', className='card-title', style=CARD_TEXT_STYLE),
-        dl.Map(dl.TileLayer(), center=[40.42868532391918, -86.91529455097324], style={'width': '100%', 'height': '100%'}, zoom=15,)],
+        dl.Map([dl.TileLayer(), dl.MarkerClusterGroup(id='markers', children=markers)], center=[40.42868532391918, -86.91529455097324], style={'width': '100%', 'height': '100%'}, zoom=15,)],
         style={'margin-bottom': '5%', 'margin-top': '5%', 'margin-right': '5%', 'margin-left': '5%', 'width': '90%', 'height': '400px'})
 ])
 
